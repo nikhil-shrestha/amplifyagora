0178260cb0685291d0299859d341eedb6533d1dc
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-17 16:06:29 +0545

    sending email with AWS SES

--- amplify/backend/function/orderlambda/src/app.js
+++ amplify/backend/function/orderlambda/src/app.js
@@ -3,6 +3,16 @@ var bodyParser = require('body-parser');
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 require('dotenv').config();
 var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+var AWS = require('aws-sdk');
+
+const config = {
+  accessKeyId: 'AKIAVDOGA75UAUKO77NN',
+  secretAccessKey: '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',
+  region: 'eu-west-2',
+  adminEmail: 'nikhil.shrestha1995@gmail.com',
+};
+
+var ses = new AWS.SES(config);
 
 // declare a new express app
 var app = express();
@@ -19,11 +29,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-/****************************
- * Example post method *
- ****************************/
-
-app.post('/charge', async (req, res) => {
+const chargeHandler = async (req, res, next) => {
   const { token } = req.body;
   const { currency, amount, description } = req.body.charge;
 
@@ -34,11 +40,53 @@ app.post('/charge', async (req, res) => {
       currency,
       description,
     });
-    res.json(charge);
+    if (charge.status === 'succeeded') {
+      req.charge = charge;
+      req.description = description;
+      next();
+    }
   } catch (err) {
     res.status(500).json({ error: err });
   }
-});
+};
+
+const emailHandler = (req, res) => {
+  const { charge } = req;
+  ses.sendEmail(
+    {
+      Source: config.adminEmail,
+      ReturnPath: config.adminEmail,
+      Destination: {
+        ToAddresses: [config.adminEmail],
+      },
+      Message: {
+        Subject: {
+          Data: 'Order Details - AmplifyAgora',
+        },
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: '<h3>Order Processed!</h3>',
+          },
+        },
+      },
+    },
+    (err, data) => {
+      if (err) return res.status(500).json({ error: err });
+      res.json({
+        message: 'Order Processed sucessfully',
+        charge,
+        data,
+      });
+    }
+  );
+};
+
+/****************************
+ * Example post method *
+ ****************************/
+
+app.post('/charge', chargeHandler, emailHandler);
 
 app.listen(3000, function () {
   console.log('App started');
